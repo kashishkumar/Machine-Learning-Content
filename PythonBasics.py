000898f2ef5674d05211c4e783edd5e28e6ee1e6
Kashish Kumar <34455806+kashishkumar@users.noreply.github.com>
2019-11-18 18:10:34 +0530

    Add files via upload

---
+++ PythonBasics.py
@@ -0,0 +1,37 @@
+'Ola'*3
+'Ola'+'Rocks'
+'Ola'.upper()
+len('Ola')
+len(str(20))
+int(20.5)
+name='Ola'
+print(name)
+list=[1,23,5,6,7,'name']
+list[5]
+len(list)
+list.reverse()
+print(list)
+dict={'key1':2,'key2':3,'key3':'Hey'}
+dict['key3']
+len(dict)
+6<7
+yes=True
+if (2>4):
+    print('True')
+else:
+    print('False')
+def hi():
+    print('Hi there')
+hi()
+for name in list:
+    print(name)
+for i in range(2,10,2):
+    print(i)
+
+class Classname(object):
+	def __init__(self,argument):
+		self.variable=argument
+	def method(self, method_argument):
+		pass
+
+
