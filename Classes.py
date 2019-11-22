e92457ff38d7c2a812bc5aa098f90fafc28c667f
Kashish Kumar <34455806+kashishkumar@users.noreply.github.com>
2019-11-22 17:30:49 +0530

    Update and rename classes.py to Classes.py

---
+++ Classes.py
@@ -0,0 +1,24 @@
+# Class Definition
+class classname:
+	# Constructor
+	def	__init__(self):	
+		self.data=[]
+	def method(self,argument):
+			
+# Object creation
+x=classname()
+x.method(arguments)
+
+# Inheritance
+class DerivedClassName(BaseClassName):
+
+#Creating a private member
+__identifier - non public 
+
+
+ 
+
+
+
+
+	
